31ea06b4dcb5bf049ad491227399664ee73c3cdb
André Luiz <142675217+andreluizdev01@users.noreply.github.com>
2023-09-10 21:42:30 -0300

    Add files via upload

---
+++ Ativ-Helio/Ativ-03/src/if_else_ativ/IF_ELSE_ativ.java
@@ -0,0 +1,149 @@
+
+package if_else_ativ;
+import javax.swing.JOptionPane;
+public class IF_ELSE_ativ {
+ public static void main(String[] args) {
+/* 
+ Double dis,temp,vel;
+ String sdis, stemp;
+ sdis=JOptionPane.showInputDialog(null, "Digite Distancia");
+ stemp=JOptionPane.showInputDialog (null, "Digite o tempo");
+ dis=Double.parseDouble(sdis);
+ temp=Double.parseDouble(stemp);
+ vel=dis/temp;
+ if(vel >= 110)
+ {
+     JOptionPane.showMessageDialog(null, "Acima");
+ }
+ else
+ {
+  JOptionPane.showMessageDialog(null, "Abaixo");
+ }
+ */
+/*
+Double temp;
+String stemp;
+stemp = JOptionPane.ShowInpuntDialog(null,"Digite a temperatura");
+Double.parseDouble(stemp);
+if(temp >= 36.5)
+ {
+     JOpionPane.ShowMessageDialog(null, "Com febre");
+ }
+ else
+ {
+  JOpitionPane.ShowMessageDIalog(null, "Sem febre");
+ }
+*/
+
+/*
+int n1, res;
+String sn1;
+sn1 = JOptionPane.showInputDialog(null, "digite um numero");
+n1 = Integer.parseInt(sn1);
+res = n1 % 7;
+if (res == 0) {
+JOptionPane.showMessageDialog(null, "multiplo de 7");
+} else {
+JOptionPane.showMessageDialog(null, "não é multiplo de 7");
+}
+*/
+
+/*
+int n1;
+String sn1;
+sn1=JOptionPane.showInputDialog(null, "digite um número");
+n1=Integer.parseInt(sn1);
+if((n1 % 2 == 0) && (n1 % 3 == 0))
+ {
+     JOptionPane.showMessageDialog(null, "par divisivel por 3");
+ }
+ else
+ {
+  JOptionPane.showMessageDialog(null, "não divisivel");
+ }
+*/
+
+/*int id;
+String id1;
+id1=JOptionPane.showInputDialog(null, "digite sua idade");
+id=Integer.parseInt(id1);
+if((id >= 18) && (id <= 65))
+ {
+     JOptionPane.showMessageDialog(null, "idade certa");
+ }
+ else
+ {
+  JOptionPane.showMessageDialog(null, "idade incorreta");
+ }
+*/
+ /*
+String sexo;
+sexo=JOptionPane.showInputDialog(null, "digite seu sexo");
+if(sexo.equals("m"))
+ {
+     JOptionPane.showMessageDialog(null, "Masculino");
+ }
+ else
+ {
+  JOptionPane.showMessageDialog(null, "Feminino");
+ }
+*/
+
+/*int p1,p2;
+String sp1,sp2;
+sp1=JOptionPane.showInputDialog(null, "digite o primeir preço");
+sp2=JOptionPane.showInputDialog(null, "digite o segundo preço");
+p1=Integer.parseInt(sp1);
+p2=Integer.parseInt(sp2);
+if(p1 > p2)
+ {
+     JOptionPane.showMessageDialog(null, "Blusa mais cara");
+ }
+ else
+ {
+  JOptionPane.showMessageDialog(null, "BLusa mais barata");
+ }
+*/
+
+/*
+String vogal;
+vogal=JOptionPane.showInputDialog(null, "digite uma letra");
+if((vogal.equals("A")) || (vogal.equals("E")) || (vogal.equals("I")) || (vogal.equals("O")) || (vogal.equals("U")))
+ {
+     JOptionPane.showMessageDialog(null, "Vogal");
+ }
+ else
+ {
+  JOptionPane.showMessageDialog(null, "Consoante");
+ }
+*/
+ /* 
+Double x1,y2;
+String sx1,sy2;
+sx1=JOptionPane.showInputDialog(null, "digite o número");
+sy2=JOptionPane.showInputDialog(null, "digite o número");
+x1=Double.parseDouble(sx1);
+y2=Double.parseDouble(sy2);
+if(x1 % y2 == 0)
+ {
+     JOptionPane.showMessageDialog(null, "É divisivel");
+ }
+ else
+ {
+  JOptionPane.showMessageDialog(null, "Não é divisivel");
+ }
+*/
+/*
+int idade, calc;
+String sIdade;
+sIdade = JOptionPane.showInputDialog(null, "digite o ano de nascimento");
+idade = Integer.parseInt(sIdade);
+calc = 2023 - idade;
+if (calc < 18) {
+JOptionPane.showMessageDialog(null, "não pode votar");
+} else {
+JOptionPane.showMessageDialog(null, "pode votar";
+*/
+    }
+    
+}
